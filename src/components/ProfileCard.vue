557e340cf5e2116b08f64eac8144b1b6a64a3fa3
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-06-22 10:54:07 -0500

    Components created

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,28 @@
+/* 
+* Inside the script tag we declare the imports
+* and the component logic
+*/
+<script setup>
+    const developer = {
+        name: "Cristian Camilo",
+        role: "Full Stack Developer",
+        country: "Colombia 🇨🇴"
+    };
+</script>
+
+/*
+* Inside the template tag we declare the HTML
+* elements inside the component
+*/
+<template>
+    <div class="profile-card">
+        <img 
+            src="https://litslink.com/wp-content/uploads/2024/06/8-1024x682.jpg" 
+            alt="Profile picture"
+        >
+
+        <h2>{{ developer.name }}</h2>
+        <p>{{ developer.role  }}</p>
+        <span>{{ developer.country }}</span>
+    </div>
+</template>
