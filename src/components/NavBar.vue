0ef78c8d6f157f64b67b1ba3de1bfd85333ae7e8
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-06-24 10:31:10 -0500

    Project card component created

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar">
+
+        <h2>Vue Dashboard</h2>
+
+        <div>
+
+            <RouterLink
+                to="/"
+            >
+                Dashboard
+            </RouterLink>
+
+            <RouterLink
+                to="/projects"
+            >
+                Projects
+            </RouterLink>
+
+        </div>
+
+    </nav>
+</template>
